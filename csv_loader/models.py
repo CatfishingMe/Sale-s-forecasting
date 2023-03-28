d3645b68b6fd2783ec2d427384e450bd779642ff
CatfishingMe <golovatskayamv@gmail.com>
2023-03-28 21:53:43 +0300

    Adding the Datasets model

--- csv_loader/models.py
+++ csv_loader/models.py
@@ -7,3 +7,11 @@ class Dealers(models.Model):
     dealer_region = models.CharField(max_length=50,
                                      verbose_name='Введите регион дилера')
     dealer_email = models.EmailField()
+
+
+class Datasets(models.Model):
+    dataset_name = models.CharField(max_length=50,
+                                    verbose_name='Введите название датасета')
+    created_date = models.DateField(auto_now_add=True,
+                                    verbose_name='Дата создания датасета')
+    dealer_id = models.ForeignKey('Dealers', on_delete=models.CASCADE)
